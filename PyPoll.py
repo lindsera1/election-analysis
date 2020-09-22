4ebdcce18d7c53b33bbbf4c4b9d190b674b58722
lindsera1 <52899223+lindsera1@users.noreply.github.com>
2020-09-21 23:21:20 -0500

    adding analysis files.

---
+++ PyPoll.py
@@ -0,0 +1,17 @@
+#%%
+import csv
+import os
+
+file_to_load = os.path.join('Resources','election_results.csv')
+
+file_to_save = os.path.join("analysis","election_analysis.txt")
+
+with open(file_to_load) as election_data:
+
+    file_reader = csv.reader(election_data)
+
+
+# %%
+for row in file_reader:
+    print(row)
+# %%
